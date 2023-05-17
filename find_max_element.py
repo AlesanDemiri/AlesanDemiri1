26e3e1cbd4f2fee01e5cb62f4b46a153e957b8e6
AlesanDemiri <133199744+AlesanDemiri@users.noreply.github.com>
2023-05-17 13:47:18 +0200

    Update find_max_element.py

--- find_max_element.py
+++ find_max_element.py
@@ -1,1 +1,21 @@
+def find_max_element(lst):
+    if not lst:
+        return None
+    max_element = lst[0]
+    for num in lst:
+        if num > max_element:
+            max_element = num
+    return max_element
 
+# Prompt the user to enter a list of numbers
+input_list = input("Enter a list of numbers separated by spaces: ")
+numbers = input_list.split()
+
+# Convert the input list to integers
+numbers = [int(num) for num in numbers]
+
+# Find the maximum element
+max_num = find_max_element(numbers)
+
+# Display the result
+print(f"The maximum element in the list is: {max_num}")
